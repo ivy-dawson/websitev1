6918d9baf15f565ed224e00b4e842e317bc46a5b
macbook <brian.dawson98@gmail.com>
2020-02-15 13:12:36 -0500

    login and logout stuff

--- untitled2/settings.py
+++ untitled2/settings.py
@@ -126,3 +126,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
