90c3b5b99e4a6742c8cf9765fc6e5663d9e92368
macbook <brian.dawson98@gmail.com>
2020-03-02 13:48:18 -0500

    home page stuff

--- untitled2/urls.py
+++ untitled2/urls.py
@@ -18,11 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('projects/', include("projects.urls")),
-    path('blog/', include("blog.urls")),
-    path('', include("projects.urls")),
-    path('accounts/', include('django.contrib.auth.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  # path('projects/', include("projects.urls")),
+                  # path('blog/', include("blog.urls")),
+                  path('', include("home.urls")),
+                  path('accounts/', include('django.contrib.auth.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
